a217913a2d8716a0c09f0b0eb747935e35b18960
Student <example@example.com>
2020-01-15 16:17:57 -0800

    [Feature] - ADd result amount filtering

--- src/views/DataGraph/components/FindWordData.js
+++ src/views/DataGraph/components/FindWordData.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Segment, Icon, Button, Form, Transition } from 'semantic-ui-react';
+import { Segment, Icon, Button, Form, Transition, Radio } from 'semantic-ui-react';
 
 import { addNode, removeNode } from "../../../actions/index";
 import { connect } from "react-redux";
@@ -13,7 +13,8 @@ class FindWordData extends React.Component {
             isLoading: true,
             data: [],
             searchQuery: '',
-            formVisible: false
+            formVisible: false,
+            maxResult: 5,
         }
     }
 
@@ -53,43 +54,9 @@ class FindWordData extends React.Component {
         });
     }
 
-    addNodesToGraph = () => {
 
 
-        let synonyms = {
-            node: {
-                id: "synonyms",
-                label: "Synonyms",
-                size: 20,
-                color: "#6699ff",
-                type: 'diamond',
-            },
-            edge: {
-                id: "synonyms",
-                source: 'main-node',
-                target: 'synonyms',
-                type: 'dashed',
-                color: "#6699ff",
-            }
-        }
-
-        let antonyms = {
-            node: {
-                id: "antonyms",
-                label: "Antonyms",
-                size: 20,
-                color: "#ff6666",
-                type: 'diamond',
-            },
-            edge: {
-                id: "antonyms",
-                source: 'main-node',
-                target: 'antonyms',
-                type: 'dashed',
-                color: "#ff6666",
-            }
-        }
-
+    handleAddRhymesNodes = () => {
         let rhymes = {
             node: {
                 id: "rhymes",
@@ -97,6 +64,8 @@ class FindWordData extends React.Component {
                 size: 20,
                 color: "#66ffcc",
                 type: 'diamond',
+                x: 0,
+                y: 0
             },
             edge: {
                 id: "rhymes",
@@ -106,39 +75,8 @@ class FindWordData extends React.Component {
                 color: "#66ffcc",
             }
         }
-
-        let associated = {
-            node: {
-                id: "associated",
-                label: "Associated",
-                size: 20,
-                color: "#ffcc66",
-                type: 'diamond',
-            },
-            edge: {
-                id: "associated",
-                source: 'main-node',
-                target: 'associated',
-                type: 'dashed',
-                color: "#ffcc66",
-            }
-        }
-
-
-        this.props.addNode(synonyms)
-        this.props.addNode(antonyms)
-        this.props.addNode(associated)
         this.props.addNode(rhymes)
 
-
-
-
-
-
-
-    }
-
-    handleAddRhymesNodes = () => {
         if (this.state.rhymes && this.state.rhymes.length > 0) {
             this.state.rhymes.forEach((word) => {
                 this.delay(100)
@@ -148,6 +86,8 @@ class FindWordData extends React.Component {
                         label: word.word,
                         type: 'circle',
                         size: 1,
+                        x: 0,
+                        y: 0
                     },
                     edge: {
                         id: word.word + 'edge' + 'rhymes',
@@ -161,6 +101,28 @@ class FindWordData extends React.Component {
     }
 
     handleAddAssociatedNodes = () => {
+
+        let associated = {
+            node: {
+                id: "associated",
+                label: "Associated",
+                size: 20,
+                color: "#ffcc66",
+                type: 'diamond',
+                x: 0,
+                y: 0
+            },
+            edge: {
+                id: "associated",
+                source: 'main-node',
+                target: 'associated',
+                type: 'dashed',
+                color: "#ffcc66",
+            }
+        }
+
+        this.props.addNode(associated)
+
         if (this.state.associated && this.state.associated.length > 0) {
             this.state.associated.forEach((word) => {
                 this.delay(100)
@@ -170,6 +132,8 @@ class FindWordData extends React.Component {
                         label: word.word,
                         type: 'circle',
                         size: 1,
+                        x: 0,
+                        y: 0
                     },
                     edge: {
                         id: word.word + 'edge' + 'associated',
@@ -183,6 +147,27 @@ class FindWordData extends React.Component {
 
     handleAddAntonymsNodes = () => {
 
+        let antonyms = {
+            node: {
+                id: "antonyms",
+                label: "Antonyms",
+                size: 20,
+                color: "#ff6666",
+                type: 'diamond',
+                x: 0,
+                y: 0
+            },
+            edge: {
+                id: "antonyms",
+                source: 'main-node',
+                target: 'antonyms',
+                type: 'dashed',
+                color: "#ff6666",
+            }
+        }
+
+        this.props.addNode(antonyms)
+
         if (this.state.antonyms && this.state.antonyms.length > 0) {
             this.state.antonyms.forEach((word) => {
                 this.delay(100)
@@ -192,6 +177,8 @@ class FindWordData extends React.Component {
                         label: word.word,
                         type: 'circle',
                         size: 1,
+                        x: 0,
+                        y: 0
                     },
                     edge: {
                         id: word.word + 'edge' + 'antonyms',
@@ -204,8 +191,31 @@ class FindWordData extends React.Component {
     }
 
     handleAddSynonymsNodes = () => {
+
+        let synonyms = {
+            node: {
+                id: "synonyms",
+                label: "Synonyms",
+                size: 20,
+                color: "#6699ff",
+                type: 'diamond',
+                x: 0,
+                y: 0
+            },
+            edge: {
+                id: "synonyms",
+                source: 'main-node',
+                target: 'synonyms',
+                type: 'dashed',
+                color: "#6699ff",
+            }
+        }
+
+        this.props.addNode(synonyms)
+
         if (this.state.synonyms && this.state.synonyms.length > 0) {
             this.state.synonyms.forEach((word) => {
+                let i = 0
                 this.delay(100)
                 this.props.addNode({
                     node: {
@@ -213,6 +223,8 @@ class FindWordData extends React.Component {
                         label: word.word,
                         type: 'circle',
                         size: 1,
+                        x: i,
+                        y: i
                     },
                     edge: {
                         id: word.word + 'edge' + 'SYNONYMS',
@@ -220,12 +232,27 @@ class FindWordData extends React.Component {
                         target: word.word
                     }
                 });
+                i = i + 1
             })
         }
+    }
+    handleDeleteNode = (incomingNode) => {
+        const edgesToRemove = this.props.dashboardGraph.graphToDisplay.edges.filter(edge => edge.source === incomingNode.id)
+        let nodesToRemove = []
+        edgesToRemove.forEach(edge => nodesToRemove = [...nodesToRemove, ...this.props.dashboardGraph.graphToDisplay.nodes.filter(node => node.id === edge.target)])
+        this.props.removeNode(incomingNode)
+        if (nodesToRemove.length > 0) {
+            nodesToRemove.forEach(node => this.handleDeleteNode(node))
+        }
+
     }
 
     handleQueryWord = (term) => {
 
+        if (this.props.dashboardGraph.graphToDisplay.nodes.find(node => node.id === "main-node")) {
+            this.handleDeleteNode(this.props.dashboardGraph.graphToDisplay.nodes.find(node => node.id === "main-node"))
+        }
+        this.delay(200)
         this.props.addNode({
             node: {
                 id: "main-node",
@@ -233,6 +260,8 @@ class FindWordData extends React.Component {
                 size: 30,
                 color: "#ffffff",
                 type: 'star',
+                x: 0,
+                y: 0
             },
             edge: {
                 id: "connecting-node",
@@ -243,18 +272,18 @@ class FindWordData extends React.Component {
             }
         })
 
-        this.addNodesToGraph()
+
 
         fetch(`https://api.datamuse.com/words?rel_syn=` + term)
             .then(res => res.json())
             .then(result =>
-                this.setState({ synonyms: result.slice(0, 10) })
+                this.setState({ synonyms: result.slice(0, this.state.maxResult) })
             )
 
         fetch(`https://api.datamuse.com/words?rel_ant=` + term)
             .then(res => res.json())
             .then(result =>
-                this.setState({ antonyms: result.slice(0, 10) })
+                this.setState({ antonyms: result.slice(0, this.state.maxResult) })
 
 
             )
@@ -262,14 +291,14 @@ class FindWordData extends React.Component {
         fetch(`https://api.datamuse.com/words?rel_trg=` + term)
             .then(res => res.json())
             .then(result =>
-                this.setState({ associated: result.slice(0, 10) })
+                this.setState({ associated: result.slice(0, this.state.maxResult) })
 
 
             )
         fetch(`https://api.datamuse.com/words?rel_rhy=` + term)
             .then(res => res.json())
             .then(result =>
-                this.setState({ rhymes: result.slice(0, 10) })
+                this.setState({ rhymes: result.slice(0, this.state.maxResult) })
 
 
             )
@@ -283,6 +312,7 @@ class FindWordData extends React.Component {
 
     handleSubmit = () => {
         this.handleQueryWord(this.state.queryString)
+
     }
 
 
@@ -338,38 +368,90 @@ class FindWordData extends React.Component {
                                 <Form.Input
                                     fluid
                                     name="queryString"
-
                                     value={this.state.queryString}
                                     onChange={this.handleChange}
                                     placeholder='Search Term...'
                                 />
 
                             </Form.Group>
+                            <Segment inverted>
 
+                                <h4>Result Amount</h4>
+                                <label style={{ color: 'white' }}>5</label>
+                                <Form.Field>
+
+                                    <Radio
+                                        toggle
+                                        name='maxResult'
+                                        value={5}
+                                        checked={this.state.maxResult === 5}
+                                        onChange={this.handleChange}
+                                    />
+                                </Form.Field>
+                                <label style={{ color: 'white' }}>10</label>
+                                <Form.Field>
+                                    <Radio
+                                        toggle
+                                        name='maxResult'
+                                        value={10}
+                                        checked={this.state.maxResult === 10}
+                                        onChange={this.handleChange}
+                                    />
+                                </Form.Field>
+
+                                <label style={{ color: 'white' }}>20</label>
+                                <Form.Field>
+                                    <Radio
+                                        toggle
+
+                                        name='maxResult'
+                                        value={20}
+                                        checked={this.state.maxResult === 20}
+                                        onChange={this.handleChange}
+                                    />
+                                </Form.Field>
+
+                                <label style={{ color: 'white' }}>50</label>
+                                <Form.Field>
+                                    <Radio
+                                        toggle
+
+                                        name='maxResult'
+                                        value={50}
+                                        checked={this.state.maxResult === 50}
+                                        onChange={this.handleChange}
+                                    />
+                                </Form.Field>
+
+
+
+
+                            </Segment>
                             <Button onClick={this.handleSubmit} positive content='Search' />
                         </Form>
-                        {synonyms && synonyms.length > 0 ?
+                        <Segment>
+                            <h5>Add Nodes</h5>
+                            {synonyms && synonyms.length > 0 ?
 
-                            <div>
+                                <div>
+                                    <p>Synonyms: {synonyms.length} <Icon style={{ cursor: 'pointer', color: 'green' }} onClick={this.handleAddSynonymsNodes} name="plus"></Icon> </p>
+                                </div> : <></>}
+                            {antonyms && antonyms.length > 0 ?
+                                <div>
 
-                                <p>Synonyms: {synonyms.length} <Icon style={{ cursor: 'pointer', color: 'green' }} onClick={this.handleAddSynonymsNodes} name="plus"></Icon> </p>
-                            </div> : <></>}
-                        {antonyms && antonyms.length > 0 ?
-                            <div>
+                                    <p>Antonyms: {antonyms.length} <Icon style={{ cursor: 'pointer', color: 'green' }} onClick={this.handleAddAntonymsNodes} name="plus"></Icon></p>
+                                </div> : <></>}
+                            {associated && associated.length > 0 ?
+                                <div>
 
-                                <p>Antonyms: {antonyms.length} <Icon style={{ cursor: 'pointer', color: 'green' }} onClick={this.handleAddAntonymsNodes} name="plus"></Icon></p>
-                            </div> : <></>}
-                        {associated && associated.length > 0 ?
-                            <div>
+                                    <p>Associated: {associated.length} <Icon style={{ cursor: 'pointer', color: 'green' }} onClick={this.handleAddAssociatedNodes} name="plus"></Icon></p>
+                                </div> : <></>}
+                            {rhymes && rhymes.length > 0 ?
+                                <div>
 
-                                <p>Associated: {associated.length} <Icon style={{ cursor: 'pointer', color: 'green' }} onClick={this.handleAddAssociatedNodes} name="plus"></Icon></p>
-                            </div> : <></>}
-                        {rhymes && rhymes.length > 0 ?
-                            <div>
-
-                                <p>Rhymes: {rhymes.length} <Icon style={{ cursor: 'pointer', color: 'green' }} onClick={this.handleAddRhymesNodes} name="plus"></Icon></p>
-                            </div> : <></>}
-
+                                    <p>Rhymes: {rhymes.length} <Icon style={{ cursor: 'pointer', color: 'green' }} onClick={this.handleAddRhymesNodes} name="plus"></Icon></p>
+                                </div> : <></>}
+                        </Segment>
                     </Segment>
                 </Transition>
             </div>
